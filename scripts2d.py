4435d31cfa0757a50964fa6ac44921e4f8e1c579
Ammar Eltigani <ammartigani12@gmail.com>
2023-02-26 17:03:25 -0500

    ran some random 2d experiments and saved their sumset plots

--- scripts2d.py
+++ scripts2d.py
@@ -28,12 +28,12 @@ def sum_sets(set1, set2):
     return np.unique(np.array([np.add(e1, e2) for e1 in set1 for e2 in set2]), axis=0)
 
 
-def single_sumset(A, iterations=None, plot=False):
+def single_sumset(A, iterations=None, slice=None, plot=False):
     print(f'A = {A}')
     A = process(A) if type(A) == list else A
     m, mm, b, c, k, nA, lenghts_arry, points_arry =  run_exps(A, iterations)
-    print(f'p(x) = ({m}) {mm}x^2 + {b}x + {c}  for all k >= {k}\n')
-    print(f'Lengths Array: {lenghts_arry}')
+    print(f'p(x) = ({m}) {mm}x^2 + {b}x + {c}  for all k >= {k}')
+    print(f'Lengths Array: {lenghts_arry}\n')
     if plot:
         if iterations is None:
             # plot A and nA
@@ -53,23 +53,26 @@ def single_sumset(A, iterations=None, plot=False):
                 axis[1].plot(points[simplex, 0], points[simplex, 1], 'k-')
             axis[1].set_title("nA")
         else:
+            assert slice[1] <= iterations + 1
+            iterations = slice[1] - slice[0]
             a = int(np.ceil(iterations / 2))
             _, axis = plt.subplots(a,2)
             for j in range(iterations):
                 m = 1 if j % 2 == 1 else 0
                 n = j // 2
-                hull = ConvexHull(points_arry[j])
-                points = np.array(points_arry[j])
-                axis[n,m].plot(points[:,0], points[:,1], 'o')
+                hull = ConvexHull(points_arry[j+slice[0]])
+                points = np.array(points_arry[j+slice[0]])
+                axis[n,m].plot(points[:,0], points[:,1], 'o', markersize=3)
                 for simplex in hull.simplices:
                     axis[n,m].plot(points[simplex, 0], points[simplex, 1], 'k-')
-                axis[n,m].set_title("A")
-        # elif iterations <= 3:
+                axis[n,m].set_title(f'iter = {j+slice[0]}')
+                axis[n,m].grid(color = 'gray', linestyle = '--', linewidth = 0.5)
+
+                # plt.savefig(f"2d_figures/4_416798/{slice[0]}-{slice[1]}.png", bbox_inches='tight', format="png", dpi=1200)
+
     
     plt.show()
                 
-
-
 
 def round_volume(vol):
     orig_vol = vol
@@ -106,6 +109,7 @@ def run_exps(curr_set, iterations):
             d13 = lengths_arry[i] - lengths_arry[i-1]
             d21 = d12 - d11
             d22 = d13 - d12
+            print(d22-d21)
             if d22 - d21 == 0:
                 if d21 != 2*m:
                     real_m = d21 / 2
@@ -157,18 +161,25 @@ def random_primitive_triangles():
         current = np.matmul(current, generator)
 
 
+# Carefully Created Experiements
 
-
-
-
-# single_sumset([(0,0), (1,0), (0,1)], iterations=50, plot=True)
-# single_sumset(random_set(20,3), iterations=50, plot=True)
 # random_primitive_triangles()
-
-single_sumset([(0,0), (1,0), (0,1)], iterations=4, plot=True)
+# single_sumset([(0,0), (1,0), (0,1)], iterations=4, plot=True)
 # single_sumset([(0,0), (2,0), (0,2), (1,0), (0,1), (1,1)], iterations=4, plot=True)
-single_sumset([(0,0), (3,0), (0,3), (1,0), (0,1)], iterations=4, plot=True)
+# single_sumset([(0,0), (3,0), (0,3), (1,0), (0,1)], iterations=4, plot=True)
 
+# Random Experiements
+
+# single_sumset(random_set(10,4), iterations=None, plot=True)
+# single_sumset([(0,0), (4,1), (6,7), (9,8)], iterations=None, slice=(34,38), plot=True) #primitive k >= 36
+# single_sumset([(0,0), (3,1), (7,1), (9,4)], iterations=17, slice=(14,18), plot=True) #primitive k>=16
+# single_sumset([(0,0), (5,2), (9,2), (9,8)], iterations=25, slice=(20, 26), plot=True) #not primitive (missing factor of 2 in second dimension) k >= 24
+# single_sumset([(0,0), (2,2), (2,3), (8,3)], iterations=12, slice=(8, 12), plot=True) #not primitive (missing factor of 2 in first dimension) k >= 6
+single_sumset([(0,0), (5,8), (7,0), (9,3)], iterations=75, slice=(0,8), plot=False) #primitive k >= 75 (takes a while to stabilize)
+
+
+# IDEA: before stabilizing, the examples I've seen seem to always have a polynomials whose 3rd derivative is 1 (until it becomes 0
+# once stabilized). Is this hinting at the fact that before stabilization the poly is higher dimensional?
 
 
 """
